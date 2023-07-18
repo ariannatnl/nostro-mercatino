cfecff88159985ec05c15c9d2e0aa22da2d8f875
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-07-19 00:49:14 +0200

    tolti prodotti e refactoring

--- layout.js
+++ layout.js
@@ -1,28 +1,5 @@
-import {
-  build,
-  UINode,
-  createDivWithClassName,
-  createElementWithId,
-  Product,
-  UIDesign,
-} from "./js/Dom.js";
-export {};
-
-const p1 = new UIDesign({ tag: "p", id: "my-p1", className: "flex rythm" });
-const p2 = new UIDesign({ tag: "p", id: "my-p1", className: "flex rythm" });
-const div2 = new UIDesign({ tag: "div", id: "my-div", className: "flex cont" });
-const div3 = new UIDesign({ tag: "div", id: "my-div", className: "flex cont" });
-const p4 = new UIDesign({ tag: "p", id: "my-p1", className: "flex rythm" });
-const code = new UIDesign({ tag: "code", id: "", className: "asds" });
-const design = new UIDesign({
-  tag: "div",
-  className: "some",
-  id: "myid",
-});
-design.addChild(
-  div2.addChild(p1).addChild(p2).addChild(code).addChild(div3.addChild(p4))
-);
-console.log(design.element);
+import { UIDesign } from "./js/Dom.js";
+import { prodotti } from "./db/prodotti.js";
 
 const makeTestata = () => {
   const header = new UIDesign({
@@ -42,7 +19,7 @@ const makeTestata = () => {
   return testata.element;
 };
 
-const makeProductScroll = () => {
+const makeProductContainer = () => {
   const makeOption = (value, text) => {
     const option = new UIDesign({
       tag: "option",
@@ -151,7 +128,14 @@ const makefooter = () => {
   const musicplayerDesign = new UIDesign({
     tag: "div",
     className: "music-player",
-  });
+  })
+    .addChild(
+      audioPlayerDesign.addChild(source1).addChild(source2).addChild(source3)
+    )
+    .addChild(trackTitleDesign)
+    .addChild(trackArtistDesign)
+    .addChild(playPauseBtnDesign)
+    .addChild(nextBtnDesign);
   const groupChatDesign = new UIDesign({
     tag: "button",
     className: "group-chat",
@@ -161,149 +145,54 @@ const makefooter = () => {
     tag: "footer",
     className: "footer",
   })
-    .addChild(
-      musicplayerDesign
-        .addChild(
-          audioPlayerDesign
-            .addChild(source1)
-            .addChild(source2)
-            .addChild(source3)
-        )
-        .addChild(trackTitleDesign)
-        .addChild(trackArtistDesign)
-        .addChild(playPauseBtnDesign)
-        .addChild(nextBtnDesign)
-    )
+    .addChild(musicplayerDesign)
     .addChild(groupChatDesign);
   return footerDesign.element;
 };
+
 const layout = document.getElementById("layout");
-const productContainer = document.getElementById("product-container");
+// testata
 const testataDesign = makeTestata();
-const productScrollDesign = makeProductScroll();
-console.log(productScrollDesign);
+layout.appendChild(testataDesign);
+// product container
+const productContainer = document.getElementById("product-container");
+const productScroll = makeProductContainer();
+for (let k in prodotti) {
+  productScroll.appendChild(prodotti[k].element);
+}
+productContainer.appendChild(productScroll);
+// footer
 const footerDesign = makefooter();
-// productContainer.appendChild(productScrollDesign);
 layout.appendChild(footerDesign);
-// layout.appendChild(testata.element);
-const productScroll = document.getElementById("product-scroll");
 const closeIcon = document.getElementById("close-icon");
 const groupChat = document.getElementById("group-chat");
 const chatSend = document.getElementById("chat-send");
 const tagsSelect = document.getElementById("tags-select");
 
-closeIcon.addEventListener("click", closeChat);
-groupChat.addEventListener("click", showChat);
-chatSend.addEventListener("click", sendMessage);
-tagsSelect.addEventListener("change", filterProducts);
-
-const soundSys = new Product({
-  dataTag: "soundsystem",
-  src: ``,
-  title: "SOUNDSYSTEM",
-  description: `Vendo causa trasloco sedia Vintage super ricercata, usata
-  pochissimo. Paglia non rovinata, puzza un pò perchè era in
-  cantina, c'è un po di umidità. Spedizione esclusa.`,
-  tags: [`#comodissima`, `#poco-usata`, `#1970`, `#ottime-rifiniture`],
-});
-const viaggi = new Product({
-  dataTag: "viaggiare",
-  src: ``,
-  title: "Viaggi a piu non posso  ",
-  description: `Vendo causa trasloco sedia Vintage super ricercata, usata
-  pochissimo. Paglia non rovinata, puzza un pò perchè era in
-  cantina, c'è un po di umidità. Spedizione esclusa.`,
-  tags: [`#comodissima`, `#poco-usata`, `#1970`, `#ottime-rifiniture`],
-});
-const viniliUsati = new Product({
-  dataTag: "musica",
-  src: ``,
-  title: "Vinili Usati x dj ",
-  description: `Vendo causa trasloco sedia Vintage super ricercata, usata
-  pochissimo. Paglia non rovinata, puzza un pò perchè era in
-  cantina, c'è un po di umidità. Spedizione esclusa.`,
-  tags: [`#comodissima`, `#poco-usata`, `#1970`, `#ottime-rifiniture`],
-});
-const volanteGaming = new Product({
-  dataTag: "elettronica",
-  src: `https://ipfs.io/ipfs/QmRiFrbk2FZU1Gd6pE949NkzX5bjeWtrgQKaJHh3xaEeUn?filename=WhatsApp%20Image%202023-07-16%20at%2015.59.25.jpeg`,
-  title: "Volante Gaming ",
-  description: `Vendo causa trasloco sedia Vintage super ricercata, usata
-  pochissimo. Paglia non rovinata, puzza un pò perchè era in
-  cantina, c'è un po di umidità. Spedizione esclusa.`,
-  tags: [`#comodissima`, `#poco-usata`, `#1970`, `#ottime-rifiniture`],
-});
-const sedia = new Product({
-  dataTag: "mobili",
-  src: `https://ipfs.io/ipfs/QmVtvHzsdXbsKCaXGP7WkUj9b5XYDdg5K4LWjhyaZa4Jyx?filename=graffiti-chair.jpg`,
-  title: "Sedia Vintage",
-  description: `Vendo causa trasloco sedia Vintage super ricercata, usata
-  pochissimo. Paglia non rovinata, puzza un pò perchè era in
-  cantina, c'è un po di umidità. Spedizione esclusa.`,
-  tags: [`#comodissima`, `#poco-usata`, `#1970`, `#ottime-rifiniture`],
-});
-const foto = new Product({
-  dataTag: "mobili",
-  src: `https://ipfs.io/ipfs/QmaHmnyKGenoP3HtY37hfCxzS6awqMzAjGtAv5HTx3jdvS?filename=Group-1601.jpg`,
-  title: "Foto Vintage",
-  description: ` Vendo causa trasloco foto Vintage super ricercata, usata
-  pochissimo. Non rovinata, molto interessante, puzza un pò perchè
-  era in cantina, c'è un po di umidità. Spedizione esclusa.`,
-  tags: [`#bellisma`, `#molto-vecchi`, `#1901`, `#ottimo-quadretto`],
-});
-
-const createProduct = () => {
-  const product = new UINode(createDivWithClassName("product"));
-  const image = new UINode(createElementWithId("img")("product-image"));
-  const description_container = new UINode(
-    createDivWithClassName("description-container")
-  );
-  const details = new UINode(createDivWithClassName("details"));
-  details.value.setAttribute("data-tags", "mobili");
-  const title = new UINode(createDivWithClassName("title"));
-  const description = new UINode(createDivWithClassName("description"));
-  return build(
-    product
-      .addChild(image)
-      .addChild(
-        description_container
-          .addChild(details)
-          .addChild(title)
-          .addChild(description)
-      )
-  );
-};
-
-productScroll.appendChild(soundSys.element);
-productScroll.appendChild(viaggi.element);
-productScroll.appendChild(viniliUsati.element);
-productScroll.appendChild(volanteGaming.element);
-productScroll.appendChild(foto.element);
-productScroll.appendChild(sedia.element);
-productScroll.appendChild(createProduct());
-
-var chatSection = document.getElementById("chatsection");
+const chatSection = document.getElementById("chatsection");
 chatSection.style.display = "none";
+// MUSIC PLAYER
+const audioPlayer = document.getElementById("audio-player");
+const trackTitle = document.getElementById("track-title");
+const trackArtist = document.getElementById("track-artist");
+const playPauseButton = document.getElementById("play-pause-button");
+const nextButton = document.getElementById("next-button");
+
 function showChat() {
   console.log("ci sono");
-  var chatSection = document.getElementById("chatsection");
   chatSection.style.display = "flex";
 
   var layout = document.getElementsByClassName("layout")[0];
   if (window.innerWidth <= 768) {
     layout.style.filter = "blur(5px)";
   }
-
   var footer = document.getElementsByClassName("footer")[0];
   footer.style.pointerEvents = "none";
 }
+
 function closeChat() {
-  var chatSection = document.getElementById("chatsection");
   chatSection.style.display = "none";
-
-  var layout = document.getElementsByClassName("layout")[0];
   layout.style.filter = "none";
-
   var footer = document.getElementsByClassName("footer")[0];
   footer.style.pointerEvents = "auto";
 }
@@ -326,7 +215,7 @@ class RelayMock {
   }
 }
 const relay = new RelayMock();
-relay.on(createTextElement);
+
 function createTextElement(messageText, messageInput) {
   var chatWindow = document.getElementById("chatWindow");
   var messageContainer = document.createElement("div");
@@ -346,40 +235,7 @@ function sendMessage() {
     relay.sendToRelay(undefined, messageText);
   }
 }
-// Aggiunto event listener una volta che la pagina è completamente caricata
-window.addEventListener("load", function () {
-  var messageInput = document.getElementById("messageInput");
-  // closeIcon.onclick(() => {});
-  // Aggiunta event listener per l'evento keydown
-  messageInput.addEventListener("keydown", function (event) {
-    // Controllo se il tasto premuto è il tasto Invio (codice 13)
-    if (event.keyCode === 13) {
-      // Chiamata alla funzione sendMessage() per inviare il messaggio
-      sendMessage();
-    }
-  });
-});
-// MUSIC PLAYER
-const audioPlayer = document.getElementById("audio-player");
-const trackTitle = document.getElementById("track-title");
-const trackArtist = document.getElementById("track-artist");
-const playPauseButton = document.getElementById("play-pause-button");
-const nextButton = document.getElementById("next-button");
 
-// Aggiungi l'evento click al pulsante play/pausa
-
-const togglePlay = () => {
-  if (audioPlayer.paused) {
-    audioPlayer.play();
-    playPauseButton.innerHTML = "&#10074;&#10074;"; // Simbolo di pausa
-  } else {
-    audioPlayer.pause();
-    playPauseButton.innerHTML = "&#9658;"; // Simbolo di play
-  }
-};
-
-playPauseButton.addEventListener("click", togglePlay);
-// MENU TENDINA
 function filterProducts() {
   var selectElement = document.getElementById("tags-select");
   var selectedTag = selectElement.value;
@@ -398,8 +254,18 @@ function filterProducts() {
     }
   }
 }
-// Aggiungi l'evento click al pulsante successivo
-nextButton.addEventListener("click", function () {
+
+const togglePlay = () => {
+  if (audioPlayer.paused) {
+    audioPlayer.play();
+    playPauseButton.innerHTML = "&#10074;&#10074;"; // Simbolo di pausa
+  } else {
+    audioPlayer.pause();
+    playPauseButton.innerHTML = "&#9658;"; // Simbolo di play
+  }
+};
+
+function handleNextButtonClick() {
   const currentSource = audioPlayer.currentSrc;
   const sources = audioPlayer.getElementsByTagName("source");
   let nextIndex =
@@ -417,4 +283,23 @@ nextButton.addEventListener("click", function () {
   );
   audioPlayer.play();
   playPauseButton.innerHTML = "&#10074;&#10074;"; // Simbolo di pausa
-});
+}
+
+function handleOnLoad() {
+  var messageInput = document.getElementById("messageInput");
+  // closeIcon.onclick(() => {});
+  // Aggiunta event listener per l'evento keydown
+  messageInput.addEventListener("keydown", function (event) {
+    // Controllo se il tasto premuto è il tasto Invio (codice 13)
+    if (event.keyCode === 13) sendMessage();
+  });
+}
+
+relay.on(createTextElement);
+window.addEventListener("load", handleOnLoad);
+closeIcon.addEventListener("click", closeChat);
+groupChat.addEventListener("click", showChat);
+chatSend.addEventListener("click", sendMessage);
+tagsSelect.addEventListener("change", filterProducts);
+playPauseButton.addEventListener("click", togglePlay);
+nextButton.addEventListener("click", handleNextButtonClick);
