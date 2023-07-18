296e343707a93d21547c2623591599ebf4be3e52
ariannatnl <arianna.minucci@me.com>
2023-07-18 17:23:58 +0200

    updated javascript

--- layout.js
+++ layout.js
@@ -20,6 +20,42 @@ groupChat.addEventListener("click", showChat);
 chatSend.addEventListener("click", sendMessage);
 tagsSelect.addEventListener("change", filterProducts);
 
+const soundSys = new Product({
+  dataTag: "soundsystem",
+  src: ``,
+  title: "SOUNDSYSTEM",
+  description: `Vendo causa trasloco sedia Vintage super ricercata, usata
+  pochissimo. Paglia non rovinata, puzza un pò perchè era in
+  cantina, c'è un po di umidità. Spedizione esclusa.`,
+  tags: [`#comodissima`, `#poco-usata`, `#1970`, `#ottime-rifiniture`],
+});
+const viaggi = new Product({
+  dataTag: "viaggiare",
+  src: ``,
+  title: "Viaggi a piu non posso  ",
+  description: `Vendo causa trasloco sedia Vintage super ricercata, usata
+  pochissimo. Paglia non rovinata, puzza un pò perchè era in
+  cantina, c'è un po di umidità. Spedizione esclusa.`,
+  tags: [`#comodissima`, `#poco-usata`, `#1970`, `#ottime-rifiniture`],
+});
+const viniliUsati = new Product({
+  dataTag: "musica",
+  src: ``,
+  title: "Vinili Usati x dj ",
+  description: `Vendo causa trasloco sedia Vintage super ricercata, usata
+  pochissimo. Paglia non rovinata, puzza un pò perchè era in
+  cantina, c'è un po di umidità. Spedizione esclusa.`,
+  tags: [`#comodissima`, `#poco-usata`, `#1970`, `#ottime-rifiniture`],
+});
+const volanteGaming = new Product({
+  dataTag: "elettronica",
+  src: `https://ipfs.io/ipfs/QmRiFrbk2FZU1Gd6pE949NkzX5bjeWtrgQKaJHh3xaEeUn?filename=WhatsApp%20Image%202023-07-16%20at%2015.59.25.jpeg`,
+  title: "Volante Gaming ",
+  description: `Vendo causa trasloco sedia Vintage super ricercata, usata
+  pochissimo. Paglia non rovinata, puzza un pò perchè era in
+  cantina, c'è un po di umidità. Spedizione esclusa.`,
+  tags: [`#comodissima`, `#poco-usata`, `#1970`, `#ottime-rifiniture`],
+});
 const sedia = new Product({
   dataTag: "mobili",
   src: `https://ipfs.io/ipfs/QmVtvHzsdXbsKCaXGP7WkUj9b5XYDdg5K4LWjhyaZa4Jyx?filename=graffiti-chair.jpg`,
@@ -28,6 +64,15 @@ const sedia = new Product({
   pochissimo. Paglia non rovinata, puzza un pò perchè era in
   cantina, c'è un po di umidità. Spedizione esclusa.`,
   tags: [`#comodissima`, `#poco-usata`, `#1970`, `#ottime-rifiniture`],
+});
+const foto = new Product({
+  dataTag: "mobili",
+  src: `https://ipfs.io/ipfs/QmaHmnyKGenoP3HtY37hfCxzS6awqMzAjGtAv5HTx3jdvS?filename=Group-1601.jpg`,
+  title: "Foto Vintage",
+  description: ` Vendo causa trasloco foto Vintage super ricercata, usata
+  pochissimo. Non rovinata, molto interessante, puzza un pò perchè
+  era in cantina, c'è un po di umidità. Spedizione esclusa.`,
+  tags: [`#bellisma`, `#molto-vecchi`, `#1901`, `#ottimo-quadretto`],
 });
 
 const createProduct = () => {
@@ -51,6 +96,12 @@ const createProduct = () => {
       )
   );
 };
+
+productScroll.appendChild(soundSys.element);
+productScroll.appendChild(viaggi.element);
+productScroll.appendChild(viniliUsati.element);
+productScroll.appendChild(volanteGaming.element);
+productScroll.appendChild(foto.element);
 productScroll.appendChild(sedia.element);
 productScroll.appendChild(createProduct());
 
