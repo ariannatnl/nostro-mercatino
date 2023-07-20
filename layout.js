1b3af37fe371574e5fea0f12e4f91a6ba9b04d04
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-07-20 13:23:55 +0200

    adattato ai cambiamenti precedenti

--- layout.js
+++ layout.js
@@ -1,239 +1,44 @@
-import { App } from "./js/App.js";
 import { UIDesign } from "./js/Dom.js";
+import { Testata } from "./js/Testata.js";
+import { Content } from "./js/Content.js";
+import { Footer } from "./js/Footer.js";
+import { PopUpMenu } from "./js/PopUpMenu.js";
+import { App } from "./js/App.js";
 import { prodotti } from "./db/prodotti.js";
 
+async function handleOnLoad() {
+  console.log("loaded");
+  const iffee = (condition, cb) => {
+    if (condition) cb();
+  };
+  const keydownHandler = (event) => iffee(event.keyCode === 13, sendMessage);
+  messageInput.addEventListener("keydown", keydownHandler);
+}
+
 const app = new App(window);
-app.themeHandler = () => console.log("color theme changed");
 app.orientationHandler = () => console.log("orientation changed");
 app.on("requestedProvider", () => console.log("provider requested"));
+app.on("load", handleOnLoad);
+app.on("themeChange", () => console.log("color theme changed"));
 
-const makeTestata = () => {
-  const header = new UIDesign({
-    tag: "header",
-    id: "header",
-    className: "bg_t flex",
-  });
-  const weblnBtn = new UIDesign({
-    tag: "button",
-    id: "webln-btn",
-    className: "bg_l c_d",
-  }).setInnerText("WebLN");
-  const subheader = new UIDesign({
-    tag: "div",
-    id: "sub-header",
-    className: "bg_t sub-header",
-  });
-  const testata = new UIDesign({
-    tag: "div",
-    id: "testata",
-    className: "bg testata",
-  })
-    .addChild(header)
-    .addChild(subheader.addChild(weblnBtn));
-  return testata;
-};
-
-const makeProductContainer = () => {
-  const makeOption = (value, text) => {
-    const option = new UIDesign({
-      tag: "option",
-      className: "bg",
-    })
-      .setInnerText(text)
-      .setHtmlAttribute("value", value);
-    return option;
-  };
-  const all = makeOption("", "#All-tags");
-  const vestiti = makeOption("vestiti", "#vestiti");
-  const musica = makeOption("musica", "#musica");
-  const elettronica = makeOption("elettronica", "#elettronica");
-  const mobili = makeOption("mobili", "#mobili");
-  const viaggiare = makeOption("viaggiare", "#viaggiare");
-  const soundSys = makeOption("soundsystem", "#soundsystem");
-  const select = new UIDesign({
-    tag: "select",
-    id: "tags-select",
-    className: "bg_t",
-  });
-  const tagsDropdown = new UIDesign({
-    tag: "div",
-    className: "tags-dropdown",
-  });
-
-  const prdScroll = new UIDesign({
-    tag: "div",
-    id: "product-scroll",
-    className: "product-scroll",
-  });
-  for (let k in prodotti) {
-    prdScroll.addChild(prodotti[k].tree);
-  }
-  const productContainer = new UIDesign({
-    tag: "div",
-    id: "product-container",
-    className:
-      "bg grid p_10 pb_0 pos_f top_100 box_bb ofy_a mh_100% w_100% ch_1",
-  })
-    .addChild(
-      tagsDropdown.addChild(
-        select
-          .addChild(all)
-          .addChild(vestiti)
-          .addChild(musica)
-          .addChild(elettronica)
-          .addChild(mobili)
-          .addChild(viaggiare)
-          .addChild(soundSys)
-      )
-    )
-    .addChild(prdScroll);
-  return productContainer;
-};
-
-const makefooter = () => {
-  const setSourceAttributes = (source, src, type, title, artist) => {
-    source.setHtmlAttribute("src", src);
-    source.setHtmlAttribute("type", type);
-    source.setHtmlAttribute("track-title", title);
-    source.setHtmlAttribute("track-artist", artist);
-    return source;
-  };
-  const source1 = new UIDesign({ tag: "source", className: "bg" });
-  setSourceAttributes(
-    source1,
-    "https://ipfs.io/ipfs/QmVEQavd4Qx31QMRGpWo5bjxFEi27NDs6uWGdNzT27PriR?filename=Gotek%20-%20Tutti%20Fatti.mp3",
-    "audio/mpeg",
-    "Gotek%20-%20Tutti%20Fatti",
-    "Gotek"
-  );
-  const source2 = new UIDesign({ tag: "source", className: "bg" });
-  setSourceAttributes(
-    source2,
-    "https://ipfs.io/ipfs/QmUKNhjXbM2X4h8ZzEq12JFCntKCKYAnk4qfU6NQr2soxG?filename=Hit%20The%20Road%20Jack.mp3",
-    "audio/mpeg",
-    "Hit%20The%20Road%20Jack",
-    "Gotek"
-  );
-  const source3 = new UIDesign({ tag: "source", className: "bg" });
-  setSourceAttributes(
-    source3,
-    "https://ipfs.io/ipfs/QmWXq8zW9XBH8C8VrHQj7zgsd8jUnARqqoE55ytKiKBq8c?filename=Pappa.mp3",
-    "audio/mpeg",
-    "Pappa",
-    "Gotek"
-  );
-  const audioPlayerDesign = new UIDesign({
-    tag: "audio",
-    id: "audio-player",
-    className: "bg",
-  })
-    .setHtmlAttribute("controls")
-    .setInnerText("Your browser does not support the audio tag.");
-  const trackTitleDesign = new UIDesign({
-    tag: "h2",
-    id: "track-title",
-    className: "bg",
-  });
-  const trackArtistDesign = new UIDesign({
-    tag: "p",
-    id: "track-artist",
-    className: "bg",
-  });
-  const playPauseBtnDesign = new UIDesign({
-    tag: "button",
-    id: "play-pause-button",
-    className: "bg_l c_d",
-    // @ariannatnl non so come si usa questa string
-  }).setInnerText("▶︎");
-  const nextBtnDesign = new UIDesign({
-    tag: "button",
-    id: "next-button",
-    className: "bg_l c_d",
-  }).setInnerText("Next");
-  const musicplayerDesign = new UIDesign({
-    tag: "div",
-    className: "music-player",
-  })
-    .addChild(
-      audioPlayerDesign.addChild(source1).addChild(source2).addChild(source3)
-    )
-    .addChild(trackTitleDesign)
-    .addChild(trackArtistDesign)
-    .addChild(playPauseBtnDesign)
-    .addChild(nextBtnDesign);
-  const groupChatDesign = new UIDesign({
-    tag: "button",
-    className: "bg_l c_d h_fc as_c",
-    id: "group-chat",
-  }).setInnerText("chat");
-  const footerDesign = new UIDesign({
-    tag: "footer",
-    className: "footer bg box_bb p_5",
-  })
-    .addChild(musicplayerDesign)
-    .addChild(groupChatDesign);
-  return footerDesign;
-};
-
-const makechat = () => {
-  const closeIcon = new UIDesign({
-    tag: "div",
-    id: "close-icon",
-    className: "close-icon",
-  }).setInnerText("✖︎");
-  const chatWindow = new UIDesign({
-    tag: "div",
-    id: "chat-window",
-    className: "chat-window bg",
-  });
-  const input = new UIDesign({
-    tag: "input",
-    id: "messageInput",
-    className: "bg_gl c_l b_n w_240 p_5",
-  })
-    .setHtmlAttribute("type", "text")
-    .setHtmlAttribute("placeholder", "Scrivi un messaggio...");
-  const button = new UIDesign({
-    tag: "button",
-    id: "chat-send",
-    className: "bg p_5-10",
-  }).setInnerText("Invia");
-  const inputContainer = new UIDesign({
-    tag: "div",
-    className: "mt_10 w_240 p_5-10 flex",
-    id: "input-container",
-  });
-  const chat = new UIDesign({
-    tag: "div",
-    id: "chatsection",
-    className: "chatsection bg",
-  })
-    .addChild(closeIcon)
-    .addChild(chatWindow)
-    .addChild(inputContainer.addChild(input).addChild(button));
-  return chat;
-};
-
-const body = document.getElementsByTagName("body").item(0);
-const layout = document.getElementById("layout");
 const bg_color = new UIDesign({ tag: "div", className: "color" });
-layout.appendChild(bg_color.element);
+app.appendTo("layout", bg_color.element);
 
 // testata
-const testataDesign = makeTestata();
+const testataDesign = new Testata().tree;
 const [header, subheader] = testataDesign.children;
 const [weblnButton] = subheader.children;
 weblnButton.element.addEventListener("click", app.requestProvider);
-layout.appendChild(testataDesign.element);
+app.appendTo("layout", testataDesign.element);
 
 // prodcontainer
-const productContainer = makeProductContainer();
+const productContainer = new Content({ prodotti }).tree;
 const [tagsDropdown, productScrollDesing] = productContainer.children;
 const [selectDesign] = tagsDropdown.children;
-layout.appendChild(productContainer.element);
+app.appendTo("layout", productContainer.element);
 
 //
-const footerDesign = makefooter();
+const footerDesign = new Footer().tree;
 const [musicPlayer, groupChatDesign] = footerDesign.children;
 const [musicPlayerDesign] = footerDesign.children;
 const [
@@ -243,12 +48,13 @@ const [
   playPauseBtnDesign,
   nextBtnDesign,
 ] = musicPlayerDesign.children;
-layout.appendChild(footerDesign.element);
-const chatDesing = makechat();
+app.appendTo("layout", footerDesign.element);
+const popUpMenuDesign = new PopUpMenu().tree;
 const [closeIconDesign, chatWindowDesign, inputContainerDesign] =
-  chatDesing.children;
+  popUpMenuDesign.children;
 const [inputDesign, buttonsDesign] = inputContainerDesign.children;
-body.appendChild(chatDesing.element);
+app.appendTo("body", popUpMenuDesign.element);
+app.setBodyClassName("m_0 w_100vw h_100vh");
 
 const closeIcon = closeIconDesign.element;
 const chatWindow = chatWindowDesign.element;
@@ -257,7 +63,7 @@ const groupChat = groupChatDesign.element;
 const chatSend = buttonsDesign.element;
 const tagsSelect = selectDesign.element;
 
-const chatSection = chatDesing.element;
+const chatSection = popUpMenuDesign.element;
 // MUSIC PLAYER
 const audioPlayer = audioPlayerDesing.element;
 const trackTitle = trackTitleDesign.element;
@@ -266,7 +72,13 @@ const playPauseButton = playPauseBtnDesign.element;
 const nextButton = nextBtnDesign.element;
 // styling
 chatSection.style.display = "none";
-body.className = "m_0 w_100vw h_100vh";
+
+function closeChat() {
+  chatSection.style.display = "none";
+  layout.style.filter = "none";
+  var footer = document.getElementsByClassName("footer")[0];
+  footer.style.pointerEvents = "auto";
+}
 
 function showChat() {
   console.log("ci sono");
@@ -278,40 +90,6 @@ function showChat() {
   }
   var footer = document.getElementsByClassName("footer")[0];
   footer.style.pointerEvents = "none";
-}
-
-function closeChat() {
-  chatSection.style.display = "none";
-  layout.style.filter = "none";
-  var footer = document.getElementsByClassName("footer")[0];
-  footer.style.pointerEvents = "auto";
-}
-
-class RelayMock {
-  subscribers = [];
-  message;
-  messageInput;
-  sendToRelay(pubKey, message, signature) {
-    this.message = message;
-    this.emit();
-  }
-  emit() {
-    this.subscribers.forEach((e) => {
-      e(this.message, this.messageInput);
-    });
-  }
-  on(subscriber) {
-    this.subscribers.push(subscriber);
-  }
-}
-const relay = new RelayMock();
-
-function createTextElement(messageText, messageInput) {
-  var messageContainer = document.createElement("div");
-  messageContainer.className = "bg_vl c_d p_10 mb_5";
-  messageContainer.textContent = messageText;
-  chatWindow.insertBefore(messageContainer, chatWindow.firstChild);
-  messageInput.value = "";
 }
 
 function sendMessage() {
@@ -376,27 +154,35 @@ function handleNextButtonClick() {
   playPauseButton.innerHTML = "&#10074;&#10074;"; // Simbolo di pausa
 }
 
-async function handleOnLoad() {
-  // closeIcon.onclick(() => {});
-  console.log("loaded");
+class RelayMock {
+  subscribers = [];
+  message;
+  messageInput;
+  sendToRelay(pubKey, message, signature) {
+    this.message = message;
+    this.emit();
+  }
+  emit() {
+    this.subscribers.forEach((e) => {
+      e(this.message, this.messageInput);
+    });
+  }
+  on(subscriber) {
+    this.subscribers.push(subscriber);
+  }
+}
+const relay = new RelayMock();
 
-  // try {
-  //   const webln = await globalThis.WebLN.requestProvider();
-
-  //   console.log(webln);
-  // } catch (err) {
-  //   // Handle users without WebLN
-  //   console.log(err);
-  // }
-  // Aggiunta event listener per l'evento keydown
-  messageInput.addEventListener("keydown", function (event) {
-    // Controllo se il tasto premuto è il tasto Invio (codice 13)
-    if (event.keyCode === 13) sendMessage();
-  });
+function createTextElement(messageText, messageInput) {
+  var messageContainer = document.createElement("div");
+  messageContainer.className = "bg_vl c_d p_10 mb_5";
+  messageContainer.textContent = messageText;
+  chatWindow.insertBefore(messageContainer, chatWindow.firstChild);
+  messageInput.value = "";
 }
 
 relay.on(createTextElement);
-app.onLoadHander = handleOnLoad;
+// app.onLoadHander = handleOnLoad;
 
 closeIcon.addEventListener("click", closeChat);
 groupChat.addEventListener("click", showChat);
